c562b0eaf5f739a6522f20f6e25afc454f3d8a89
Ursula Kaczmarek <ursulakaczmarek@users.noreply.github.com>
2016-12-05 13:35:17 -0500

    Create lotto.R

---
+++ Introduction/week4.session1/lotto.R
@@ -0,0 +1,22 @@
+
+# script to create NY winning lotto dataset
+
+library(RSocrata)
+
+# import lotto numbers
+url <- "https://data.ny.gov/resource/7pxf-c5iz.json"
+
+# read the dataset into R
+lotto <- read.socrata(url)
+
+# coercing to proper data type
+lotto['cash_ball'] <- as.numeric(lotto$cash_ball)
+
+# function to split winning numbers into 5 cols
+lotto <- setNames(data.frame(lotto, do.call(rbind, 
+                                    strsplit(lotto$winning_numbers, split = " "))),
+                                    c("cashball", "drawdate", "winningnums",paste0("num", 1:5)))
+
+lotto[,4:8] <- lapply(lotto[,4:8], as.numeric)
+
+write.csv(lotto, file="lotto.csv")
