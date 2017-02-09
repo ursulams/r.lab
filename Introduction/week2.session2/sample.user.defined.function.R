a417de95f2f6a4bc8020aecc51e7405627d1ab58
Ursula Kaczmarek <ursulakaczmarek@users.noreply.github.com>
2017-02-09 11:50:18 -0500

    Update sample.user.defined.function.R

--- Introduction/week2.session2/sample.user.defined.function.R
+++ Introduction/week2.session2/sample.user.defined.function.R
@@ -1,6 +1,6 @@
 # generate a sample of size n of dates of type character vector
 ## create a sample of size 25: make.date(25)
-make.date <- function(n, first = as.Date("01-01-1900"), last = Sys.Date()) {
+make.date <- function(n, first = as.Date("1900-01-01"), last = Sys.Date()) {
   as.Date(sample.int(last - first, n), origin = first)
 }
 
